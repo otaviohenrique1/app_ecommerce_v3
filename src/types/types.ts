fdeff39991e3c2ca9c1823a227c20574fa330ffb
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2024-11-19 22:10:20 -0300

    commit 4

--- src/types/types.ts
+++ src/types/types.ts
@@ -2,3 +2,36 @@ export interface FormTypesLogin {
   email: string;
   senha: string;
 }
+
+export interface FormTypesCadastroPessoaFisica extends DadosBasicos, Endereco {
+  nome: string;
+  cpf: string;
+  genero: string;
+  dataNascimento: string;
+}
+
+export interface FormTypesCadastroPessoaJuridica extends DadosBasicos, Endereco {
+  nomeFantasia: string;
+  razaoSocial: string;
+  cnpj: string;
+  informacoesTributarias: string;
+  incricaoEstadual: string;
+  dataAberturaEmpresa: string;
+}
+
+interface DadosBasicos {
+  email: string;
+  senha: string;
+  confimarSenha: string;
+  celular: string;
+  telefone: string;
+}
+
+interface Endereco {
+  cep: string;
+  endereco: string;
+  numero: string;
+  cidade: string;
+  estado: string;
+  complemento: string;
+}
--- src/utils/constantes.ts
+++ src/utils/constantes.ts
@@ -6,7 +6,37 @@ export const valoresIniciaisLogin: FormTypesLogin = {
   senha: "",
 }
 
+const nome = yup.string().required("Campo vazio");
+const email = yup.string().required("Campo vazio").email("Email inválido");
+const senha = yup.string().required("Campo vazio");
+const confimarSenha = yup.string().required("Campo vazio");
+const cpf = yup.string().required("Campo vazio");
+const genero = yup.string().required("Campo vazio");
+const dataNascimento = yup.string().datetime("Data inválida").required("Campo vazio");
+const nomeFantasia = yup.string().required("Campo vazio");
+const razaoSocial = yup.string().required("Campo vazio");
+const cnpj = yup.number().required("Campo vazio");
+const informacoesTributarias = yup.string().required("Campo vazio");
+const incricaoEstadual = yup.string().required("Campo vazio");
+const dataAberturaEmpresa = yup.string().datetime("Data inválida").required("Campo vazio");
+const celular = yup.number().required("Campo vazio");
+const telefone = yup.number().required("Campo vazio");
+const cep = yup.number().required("Campo vazio");
+const endereco = yup.string().required("Campo vazio");
+const numero = yup.number().required("Campo vazio");
+const cidade = yup.string().required("Campo vazio");
+const estado = yup.string().required("Campo vazio");
+const complemento = yup.string().required("Campo vazio");
+
 export const schemaLogin = yup.object().shape({
-  email: yup.string().required("Campo vazio").email("Email inválido"),
-  senha: yup.string().required("Campo vazio"),
+  email, senha,
 });
+
+export const schemaCadastroPessoaFisica = yup.object().shape({
+  nome, cpf, email, senha, confimarSenha, genero, dataNascimento, celular, telefone, cep, endereco, numero, cidade, estado, complemento
+});
+
+export const schemaCadastroPessoaJuridica = yup.object().shape({
+  nomeFantasia, razaoSocial, cnpj, email, senha, confimarSenha, informacoesTributarias, incricaoEstadual, dataAberturaEmpresa, celular, telefone, cep, endereco, numero, cidade, estado, complemento
+});
+
